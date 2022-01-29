4aac35474adddd4a087c497bd64e608d81a9790a
Exenifix <yeligar@ya.ru>
2022-01-29 15:55:25 +0300

    Improvements to the main screen

--- Assets/Scripts/StartScreen/StartManager.cs
+++ Assets/Scripts/StartScreen/StartManager.cs
@@ -1,42 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class StartManager : MonoBehaviour
 {
-    public GameObject donut;
-    private bool donutsSpawningEnabled = true;
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(SpawnDonuts());
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public void StartGame() {
-        donutsSpawningEnabled = false;
+    public void StartGame()
+    {
         SceneManager.LoadScene("MainScene", LoadSceneMode.Single);
     }
 
-    public void QuitGame() {
+    public void QuitGame()
+    {
         Application.Quit();
-    }
-
-    private IEnumerator SpawnDonuts() {
-        while (donutsSpawningEnabled) {
-            yield return new WaitForSeconds(Random.Range(0.2f, 2.0f));
-            Instantiate(donut, 
-            new Vector3(
-                Random.Range(-2f, 2f),
-                Random.Range(2f, 4f),
-                0
-            ), donut.transform.rotation);
-        }
     }
 }
